822eb929d70315c84cd35d74a688f80eb5b73d4a
Fco Javier Gómez Galán <javiergomez742@gmail.com>
2025-05-31 14:55:42 +0200

    ignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseFnUrl: 'http://localhost:54321/functions/v1',
+  supabaseUrl: 'https://tcbplmfcfwkjvgrpnjxr.supabase.co',
+  supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjYnBsbWZjZndranZncnBuanhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTQyNDAsImV4cCI6MjA2MTY3MDI0MH0.uYiX2oRrgHRbU70XQrtEbHrQKyOZoQso7RSku5N3tQM'
+};
